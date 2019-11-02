21a1a1a50e59beacad6c201bb573f01b1ccbf2e8
Shana Wiltshire <shanawiltshire@gmail.com>
2019-11-02 23:45:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
   new_array[]
   counter = 0
-  while array[counter]
-  
+  while counter < array.lenght do
+    new_array << array[counter] ** 2
+    counter == 1
 end
 end
